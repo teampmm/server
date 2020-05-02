796e57112a78e5ff28b5901515865789cc34d063
ehdgnl8940 <ehdgnl8940@naver.com>
2020-05-02 12:27:32 +0900

    수정 ) readme

--- html/application/controllers/User.php
+++ html/application/controllers/User.php
@@ -4,99 +4,98 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class User extends CI_Controller
 {
-    public $http_method;
+	public $http_method;
 
-    public function __construct()
-    {
-        parent::__construct();
+	public function __construct()
+	{
+		parent::__construct();
 
-        // 클라에서 요청한 (GET, POST, PATCH, DELETE) HTTP 메서드 확인
-        $this->http_method = $_SERVER["REQUEST_METHOD"];
+		// 클라에서 요청한 (GET, POST, PATCH, DELETE) HTTP 메서드 확인
+		$this->http_method = $_SERVER["REQUEST_METHOD"];
 //        return $this->http_method;
-    }
+	}
 
-    // request url : {서버 ip}/user
-    public function index()
-    {
-        // 클라이언트가 회원가입 요청함.
-        if ($this->http_method == "POST") {
-            // postman에서 데이터를 body - x-www-form-urlencoded로 보내는 방법
-            $json_data = $this->input->post('user_info', true);
+	// request url : {서버 ip}/user
+	public function index()
+	{
+		// 클라이언트가 회원가입 요청함.
+		if ($this->http_method == "POST") {
+			// postman에서 데이터를 body - x-www-form-urlencoded로 보내는 방법
+			$json_data = $this->input->post('user_info', true);
 //            echo $json_data;
 //            return '';
-            $json_data = json_decode($json_data, true);
+			$json_data = json_decode($json_data, true);
 
-            // 클라이언트가 보낸 정보에서 name 값을 찾는 코드
-            // print_r($json_data['name']);
+			// 클라이언트가 보낸 정보에서 name 값을 찾는 코드
+			// print_r($json_data['name']);
 
-            $this->load->model("UserModel");
-            // db에 사용자를 추가한다.
-            $result = $this->UserModel->putUser($json_data);
+			$this->load->model("UserModel");
+			// db에 사용자를 추가한다.
+			$result = $this->UserModel->putUser($json_data);
 
-            echo $result;
+			echo $result;
+		}
+	}
 
-        }
-    }
+	public function headerData($jwtToken)
+	{
+	}
 
-    public function headerData($jwtToken)
-    {
-    }
+	// 닉네임 중복 체크 메서드
+	public function getNickNameCheck()
+	{
+		// 사용자가 닉네임 중복 체크 요청 - nick_name 가지고있다.
+		$json_data = $this->input->get('nick_name_request', true);
+		$json_data = json_decode($json_data, true);
+		$this->load->model('UserModel');
+		$result = $this->UserModel->getNickCheck($json_data);
+		echo($result);
+	}
 
-    // 닉네임 중복 체크 메서드
-    public function getNickNameCheck()
-    {
-        // 사용자가 닉네임 중복 체크 요청 - nick_name 가지고있다.
-        $json_data = $this->input->get('nick_name_request', true);
-        $json_data = json_decode($json_data, true);
-        $this->load->model('UserModel');
-        $result = $this->UserModel->getNickCheck($json_data);
-	    echo($result);
-    }
+	// 로그인 요청 메서드
+	public function loginRequest()
+	{
+		// 사용자가 로그인 요청 - id, pw 정보를 가지고있다.
+		$json_data = $this->input->post('login_request', true);
+		$json_data = json_decode($json_data, true);
 
-    // 로그인 요청 메서드
-    public function loginRequest()
-    {
-        // 사용자가 로그인 요청 - id, pw 정보를 가지고있다.
-        $json_data = $this->input->post('login_request', true);
-        $json_data = json_decode($json_data, true);
+		// 사용자가 보낸 id, pw 정보를 db에 있는 id, pw와 비교한다.
+		$this->load->model('UserModel');
+		$result = $this->UserModel->getLoginStatus($json_data);
+		echo($result);
+	}
 
-        // 사용자가 보낸 id, pw 정보를 db에 있는 id, pw와 비교한다.
-        $this->load->model('UserModel');
-        $result = $this->UserModel->getLoginStatus($json_data);
-	    echo($result);
-    }
+	// 클라이언트가 사용자에 대한 데이터를 요청할때
+	// request url : {서버 ip}/user/{data}
+	public function requestData($client_data)
+	{
+		// 사용자 정보를 조회 할 때
+		if ($this->http_method == "GET") {
+			if ($client_data == 'nick_name') {
+				// 사용자 닉네임 중복체크
+				$this->getNickNameCheck();
+			}
+		}
+		else if($this->http_method == "POST"){
+			// 클라이언트가 로그인 요청
+			if ($client_data == "login") {
+				$this->loginRequest();
+			}
+		}
+		else if ($this->http_method == "PATCH"){
+			print_r("PATCH");
+		}
+		else if($this->http_method == "DELETE"){
+			print_r("DELETE");
+		}
 
-    // 클라이언트가 사용자에 대한 데이터를 요청할때
-    // request url : {서버 ip}/user/{data}
-    public function requestData($client_data)
-    {
-        // 사용자 정보를 조회 할 때
-        if ($this->http_method == "GET") {
-            if ($client_data == 'nick_name') {
-                // 사용자 닉네임 중복체크
-                $this->getNickNameCheck();
-            }
-        } else {
-            if ($this->http_method == "POST") {
-                // 클라이언트가 로그인 요청
-                if ($client_data == "login") {
-                    $this->loginRequest();
-                }
-            } else {
-                if ($this->http_method == "PATCH") {
-                } else {
-                    if ($this->http_method == "DELETE") {
-                    }
-                }
-            }
-        }
-    }
+	}
 
 	//회원가입시 핸드폰 인증을 받는과정
-	function sms($client_data){
-		if($this->http_method=='POST') {
+	function sms($client_data)
+	{
+		if ($this->http_method == 'POST') {
 			if ($client_data == 'phone') {
-
 				require_once "/home/ubuntu/db/sms/lib/lib.php";
 				require_once "/home/ubuntu/db/sms/class/Clientapi.class.php";
 				$smsobj = new Clientapi();
@@ -109,24 +108,26 @@ class User extends CI_Controller
 				$check_code = $this->UserModel->phoneCheck($user_phone);
 
 				// 가입 가능
-				if ($check_code==0){
+				if ($check_code == 0) {
 					//인증코드는 4글자의 숫자
-					$auth_code=sprintf('%04d',rand(0000,9999));
-					$result_code= $smsobj->gd_sms_signal('sms', 'send', $user_phone, '01077024277', iconv('utf8', 'euckr', '인증번호는 '.$auth_code.' 입니다'), '', '', '', '', '4');
+					$auth_code = sprintf('%04d', rand(0000, 9999));
+					$result_code = $smsobj->gd_sms_signal('sms', 'send',
+						$user_phone, '01077024277',
+						iconv('utf8', 'euckr', '인증번호는 ' . $auth_code . ' 입니다'),
+						'', '', '', '', '4');
 					//sms보내기 정상적으로 왼료되면 0000코드 반환
-					if($result_code==0000){
+					if ($result_code == 0000) {
 						//클라이언트에는 인증 코드가 아닌 인증 코드를 sha256으로 변환한 코드를 준다
-						$response_code['result']=hash('sha256',$auth_code);
+						$response_code['result'] = hash('sha256', $auth_code);
 						echo json_encode($response_code);
-					}else{
-					    $response_code['result']='다음에 다시시도 서버에러';
-                        echo json_encode($response_code);
+					} else {
+						$response_code['result'] = '다음에 다시시도 서버에러';
+						echo json_encode($response_code);
 					}
-				}
-				// 가입 불가능
-				else{
-                    $response_code['result']="이미 가입한 아이디";
-                    echo json_encode($response_code);
+				} // 가입 불가능
+				else {
+					$response_code['result'] = "이미 가입한 아이디";
+					echo json_encode($response_code);
 				}
 			}
 		}

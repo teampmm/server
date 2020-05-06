8c4859fdd7897e04479c586a1c69cccaf4d90254
iamstar7277 <iamstar7277@gmail.com>
2020-05-06 16:12:40 +0900

    수정 ) 서버가 클라이언트에서 받는 데이터 모두 json 으로 통일

--- html/application/controllers/Bill.php
+++ html/application/controllers/Bill.php
@@ -24,44 +24,48 @@ class Bill extends CI_Controller
 	    if ($this->http_method=="GET") {
             //법안 모아보기
             if ($data == 'card') {
-                $page_idx = $this->input->get('page', true);
-                $result = $this->page($page_idx);
+                $page_idx = $this->input->get('page_request', true);
+                $page_idx=json_decode($page_idx,true);
+                $result = $this->page($page_idx['page']);
                 echo $result;
             } //법안 상세정보
             else if ($data == 'bill_info') {
-                $bill_idx = $this->input->get('bill_idx', true);
-                $result = $this->billInfo($bill_idx);
+                $bill_idx = $this->input->get('bill_idx_request', true);
+                $bill_idx=json_decode($bill_idx,true);
+                $result = $this->billInfo($bill_idx['bill_idx']);
                 echo $result;
             } //법안 자체의 좋아요 싫어요 수
             else if ($data == 'bill_user_evaluation') {
-                $bill_idx = $this->input->get('bill_idx', true);
-                $result = $this->billUserEvaluation($bill_idx);
+                $bill_idx = $this->input->get('bill_idx_request', true);
+                $bill_idx=json_decode($bill_idx,true);
+                $result = $this->billUserEvaluation($bill_idx['bill_idx']);
                 echo $result;
             }
             //법안의 찬성 댓글 and 대댓글 갯수
             //페이징
             else if ($data == 'bill_agreement_comment') {
-                $bill_idx = $this->input->get('bill_idx', true);
-                $comment_page = $this->input->get('comment_page', true);
-                $result = $this->billComment($bill_idx, $comment_page, 'agreement');
+                $bill_idx = $this->input->get('bill_idx_request', true);
+                $bill_idx=json_decode($bill_idx,true);
+                $result = $this->billComment($bill_idx['bill_idx'],$bill_idx['comment_page'], 'agreement');
                 echo $result;
 
             }
             //법안의 반대 댓글 and 대댓글 갯수
             //페이징
             else if ($data == 'bill_opposition_comment') {
-                $bill_idx = $this->input->get('bill_idx', true);
-                $comment_page = $this->input->get('comment_page', true);
-                $result = $this->billComment($bill_idx, $comment_page, 'opposition');
+                $bill_idx = $this->input->get('bill_idx_request', true);
+                $bill_idx=json_decode($bill_idx,true);
+                $result = $this->billComment($bill_idx['bill_idx'], $bill_idx['comment_page'], 'opposition');
                 echo $result;
             }
             //대댓글 보기 클릭
             //댓글 idx , 대댓글 페이지
             //대댓글의 정보 and 페이징
             else if ($data == 'bill_sub_comment') {
-                $comment_idx = $this->input->get('comment_idx', true);
-                $sub_comment_page = $this->input->get('sub_comment_page', true);
-                $result = $this->billSubComment($comment_idx, $sub_comment_page);
+                $comment_idx = $this->input->get('comment_idx_request', true);
+                $comment_idx=json_decode($comment_idx,true);
+
+                $result = $this->billSubComment($comment_idx['comment_idx'], $comment_idx['sub_comment_page']);
                 echo $result;
             }
         }else if ($this->http_method=="POST"){

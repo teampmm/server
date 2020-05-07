0b19329c8d17e299c8f6c344e5f1300e1220841e
iamstar7277 <iamstar7277@gmail.com>
2020-05-07 16:51:49 +0900

    추가,수정) 요청데이터 json-> query로 바꿈 , invaild_data_ 추가 , 400에러 코드 추가
    클라이언트 요청에 문법 에러가 있을시 invaild_data_[name] 이라는 문구와 함께 400 status code 반환

---
+++ html/application/controllers/DTO/Option.php
@@ -0,0 +1,14 @@
+
+<?php
+
+    function JsonNullCheck($json,$check_array){
+        foreach ($check_array as $check_data){
+            if (empty($json[$check_data])){
+                return 'invaild_data_'.$check_data;
+            }
+        }
+        return null;
+    }
+
+
+

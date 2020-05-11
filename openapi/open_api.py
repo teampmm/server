02a10d62e87697822b404438464041ead0067893
iamstar7277 <iamstar7277@gmail.com>
2020-05-11 16:45:36 +0900

    수정 ) api 결과값 txt -> 엑셀로 변경 (신동휘님 요청사항)

--- openapi/open_api.py
+++ openapi/open_api.py
@@ -1,7 +1,10 @@
 #-*- coding:utf-8 -*-
 import requests
-import re
-
+import xmltodict
+import json
+from openpyxl import Workbook
+from openpyxl.styles import Alignment
+import os
 """
 선거 코드 조회 api
 
@@ -13,43 +16,38 @@ sgTypecode == 선거종류코드 1.대통형 2.국회의원 3.시도지사 4.구
 sgVotedate == 선거일자
 """
 url = 'http://apis.data.go.kr/9760000/CommonCodeService/getCommonSgCodeList'
-data={'ServiceKey':'key','numOfRows':'1000'}
+data={'ServiceKey':"NXjYP6Ks1pRP2JTAbmAel7rQANhWPadkC8BvdLFpmDqVFsMI72sW3ukkWMm3WsXlPmucYg/fxi/WfVJQSLTzkA==",'numOfRows':'1000'}
 resp=requests.get(url,params=data)
 
+#xml형태인 반환값을 json형태로 변환
+request_data=json.dumps(xmltodict.parse(resp.text),indent=4)
+request_data=json.loads(request_data)
 
 """
-결과값이 xml로 반환되기떄문에
-xml을 보기 편하게 정리하고 .txt 파일로 저장하기 위함
+반환값을 엑셀로 저장 하기 위함
 """
-#xml에서  필요없는 데이터 자름
-text=resp.text.split("<numOfRows>")[0]
-#xml 태그 없애기 정규식
-text=text.replace("  ","")
-text = re.sub('<.+?>', '', text, 0, re.I|re.S)
+result_file=Workbook()
+write_result=result_file.active
+write_result.merge_cells('A1:H5')
 
-text=text.replace('INFO-00','')
-text=text.replace('NORMAL SERVICE','')
-text=text.replace("\n","",8)
-text=text.replace("\n\n",'')
-# text=text.replace("  ",'')
+num=13
+cell = write_result.cell(row=1, column=1)
+cell.alignment=Alignment(wrap_text=True)
 
-f=open('중앙선거관리위원회_코드정보_선거코드결과.txt','w')
-print(text)
-for i in range(len(text.split('\n'))):
-    print(text.split('\n')[i])
-    f.write('||'+text.split('\n')[i])
-    if (i+1)%5==0:
-        print('')
-        f.write('\n')
+write_result.column_dimensions['B'].width=30
+write_result.column_dimensions['C'].width=15
 
-f.close()
+write_result.cell(row=1,column=1).value='sgId = 선거 ID \nsgName = 선거명 \nsgTypcode = 선거 종류 코드 (1.대통령 2.국회의원 3.시도지사 4.구시군장 5.시도의원 6.구시군의회의원 7.국회의원비례대표 8.광역의원비례대표 9.기초의원비례대표 10.교육의원 11.교육감)\nsgVotedate = 선거일자'
+write_result.cell(row=num-1,column=1).value='선거 ID'
+write_result.cell(row=num-1,column=2).value='선거명'
+write_result.cell(row=num-1,column=3).value='선거종류코드'
+write_result.cell(row=num-1,column=4).value='선거날짜'
+for i,v in enumerate(request_data['response']['body']['items']['item']):
+    write_result.cell(row=num+i,column=1).value=v['sgId']
+    write_result.cell(row=num+i,column=2).value=v['sgName']
+    write_result.cell(row=num+i,column=3).value=v['sgTypecode']
+    write_result.cell(row=num+i,column=4).value=v['sgVotedate']
 
-#도움말.txt만들기
-surmmary=open('도움말.txt','w')
-surmmary.write('순서대로\n')
-surmmary.write('num == 인덱스 (쓸데없음)\n')
-surmmary.write('sgid == 선거ID\n')
-surmmary.write('sgName == 선거명\n')
-surmmary.write('sgTypecode == 선거종류코드 1.대통형 2.국회의원 3.시도지사 4.구시군장 5.시도의원 6.구시군의회의원 7.국회의원비례대표 8.광역의원비례대표 9.기초의원비례대표 10.교육의원 11.교육감\n')
-surmmary.write('sgVotedate == 선거일자\n')
-surmmary.close()
+if not (os.path.isdir('선거코드')):
+    os.makedirs(os.path.join('선거코드'))
+result_file.save('선거코드/선거코드.xlsx')

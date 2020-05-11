50e38e7be2d838d0a4ccc92174de8cc3e6563a37
ehdgnl8940 <ehdgnl8940@naver.com>
2020-05-11 14:25:29 +0900

    수정) createToken 정보에 사용자의 아이디, 인덱스, 닉네임 추가

--- html/application/controllers/DTO/PolicticsJwt.php
+++ html/application/controllers/DTO/PolicticsJwt.php
@@ -15,7 +15,7 @@ class PolicticsJwt
     }
 
     // 사용자가 로그인 요청 시 - 토큰 생성
-    function createToken($json){
+    function createToken($user_info){
 
         // 현재 시간 : 년도 월 일 : 20201029
         $currentTime = str_replace('-','',date("Y-m-d"));
@@ -23,15 +23,17 @@ class PolicticsJwt
         $deadlineTime = (string)((int)$currentTime + 1);
 
         /** 클라이언트에 보내는 토큰 정보
-        서버주소
-        클라이언트 id
+        사용자 id
+        사용자 인덱스
+        사용자 닉네임
         토큰 발급시간
         토근 만료시간
          */
 //        print_r($id->email_id);
         $payload = array(
-            "server url" => "http://52.78.106.225",
-            "client id" => $json['id'],
+            "idx" => $user_info->idx,
+            "id" => $user_info->id,
+            "nick_name" => $user_info->nick_name,
             "token issue time" => $currentTime,
             "token expiration time" => $deadlineTime
             //    "data" => [
@@ -45,12 +47,14 @@ class PolicticsJwt
         // 인코딩 하는 과정에서 ` = ` 이라는 문자가 포함 되는 경우도 있다.
         // 토큰은 url 파라미터도 전달되는 경우가 있다고 하는데, url-safe하지 않는다고 지워주라고 한다. // 아직 잘 모르겠음
         $jwt = str_replace('=','',$jwt);
+
         return $jwt;
     }
 
     // 사용자 정보 파싱하기
-    function tokenParsing(){
-
+    function tokenParsing($jwt){
+        $decoded = JWT::decode($jwt, $this->key, array('HS256'));
+        return $decoded;
     }
 
     // 사용자가 로그아웃 요청 시 - 토큰 제거

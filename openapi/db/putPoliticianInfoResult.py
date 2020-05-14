654d5cdff5b3b673788c54d40f7ed077255a81b1
iamstar7277 <iamstar7277@gmail.com>
2020-05-14 14:28:20 +0900

    수정) db에 문자열로 None 들어가는거 NULL로 수정 (추가) 정당 공약

--- openapi/db/putPoliticianInfoResult.py
+++ openapi/db/putPoliticianInfoResult.py
@@ -21,48 +21,51 @@ start=0
 for i in load_code.rows:
 
     sql="select * from Politician where kr_name=%s and ch_name=%s"
-    curs.execute(sql,(str(i[2].value),str(i[4].value)))
+    curs.execute(sql,((i[2].value),(i[4].value)))
     query_result=curs.fetchone()
     print(query_result)
     for j in str(i[16].value).replace("대","").split(","):
         if(start==1):
             if j=="None" or j=='20':
-                sql="INSERT INTO PoliticianInfo (politician_idx, elect_generation, elect_area, committee_idx, party_idx, office_number, affiliation_committee, email_id, email_address, aide, secretary, create_at, deptCd, num, homepage) " \
+                sql="INSERT INTO PoliticianInfo (politician_idx, elect_generation, elect_area, committee_idx, party_idx, office_number, email_id, email_address, aide, secretary, create_at,vote_score,deptCd, num, homepage) " \
                     "VALUES " \
-                    "(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, NOW(),  %s, %s, %s);"
+                    "(%s, %s, %s, %s, %s, %s, %s, %s, %s,  %s, NOW(), %s, %s, %s, %s);"
                 politician_idx=int(query_result[0])
                 elect_generation=20
-                elect_area=str(i[6].value)
-                committee_idx=str(i[14].value)
-                party_idx=str(i[12].value)
-                office_number=str(i[17].value)
-                affiliation_committee=str(i[14].value)
-                email_id=''
-                email_address=''
+                elect_area=(i[6].value)
+                committee_idx=(i[14].value)
+                party_idx=(i[12].value)
+                party_idx_sql="select * from PartyName where name=%s"
+                curs.execute(party_idx_sql,(party_idx))
+                party_idx=curs.fetchone()
+                party_idx=party_idx[0]
+                office_number=(i[17].value)
+                email_id=None
+                email_address=None
                 try:
                     email_id=str(i[19].value).split("@")[0]
                     email_address=str(i[19].value).split("@")[1]
                 except IndexError:
                     pass
-                aide=str(i[20].value).replace(" ","")
-                secretary=str(str(i[21].value)+","+str(i[22].value)).replace(" ","")
+                aide=(i[20].value).replace(" ","")
+                secretary=((i[21].value)+","+(i[22].value)).replace(" ","")
 
                 deptCd=int(i[0].value)
                 num=int(i[1].value)
 
-
+                vote_score=(i[46].value)
 
 
                 homepage=str(i[18].value)
-                print(politician_idx,elect_generation,elect_area,committee_idx,party_idx,office_number,affiliation_committee,email_id,email_address)
+                print(politician_idx,elect_generation,elect_area,committee_idx,party_idx,office_number,email_id,email_address)
                 print(aide)
                 print(secretary)
                 print(deptCd,num)
-                curs.execute(sql, (politician_idx,elect_generation,elect_area,committee_idx,party_idx,office_number,affiliation_committee,email_id,email_address,aide,secretary
-                                   ,deptCd,num,homepage))
+                curs.execute(sql, (politician_idx,elect_generation,elect_area,committee_idx,party_idx,office_number,email_id,email_address,aide,secretary
+                                   ,vote_score,deptCd,num,homepage))
 
             else :
-                sql="insert into PoliticianInfo(politician_idx,elect_generation) values (%s,%s)"
+                sql="insert into PoliticianInfo(politician_idx,elect_generation,create_at) values (%s,%s,NOW())"
                 curs.execute(sql,(int(query_result[0]),int(j)))
         if (i[0].value == '부서코드'):
             start = 1
--- openapi/db/putPoliticianResult.py
+++ openapi/db/putPoliticianResult.py
@@ -6,15 +6,7 @@ from db import dbInfo
 정치인 정보를 db에 넣는 스크립트
 국회의원현황/국회의원.xlsx 가 있어야함
 """
-"""
-한글이름
-한자이름
-영어이름
-약력
-생년월일
-이미지
-생성된때
-"""
+
 host,user,password,db,charset=dbInfo.디비정보()
 
 conn=pymysql.connect(host=host,user=user,password=password,db=db,charset=charset)
@@ -29,7 +21,8 @@ for i in load_code.rows:
     if (start==1):
         print(i[0].value)
         sql = "INSERT INTO Politician (kr_name,ch_name, en_name, history, birthday, profile_image_url, create_at) VALUES (%s, %s, %s,%s,%s,%s, NOW());"
-        curs.execute(sql,(str(i[2].value),str(i[4].value),str(i[3].value),str(i[25].value),int(i[11].value),str(i[7].value)))
+        curs.execute(sql,((i[2].value),(i[4].value),(i[3].value),(i[25].value),int(i[11].value),(i[7].value)))
+
     if (i[0].value=='부서코드'):
         start=1
 conn.commit()
